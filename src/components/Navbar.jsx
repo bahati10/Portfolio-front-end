f0a14e29dc506eed5efb12bc6377f0909fd22b7e
Bahati Yves <bahatiyves10@gmail.com>
2023-11-25 22:59:15 +0200

    Test for home page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { Button, Typography } from "@mui/material";
+
+const Navbar = () => {
+  return (
+    <>
+      <Button>
+        <Typography>Button 1</Typography>
+      </Button>
+      <Button>
+        <Typography>Button 1</Typography>
+      </Button>
+      <Button>
+        <Typography>Button 1</Typography>
+      </Button>
+      <Button>
+        <Typography>Button 1</Typography>
+      </Button>
+    </>
+  );
+};
+
+export default Navbar;
