c9032a053cd8acfadf4dd08b3430526232e81872
bahati yves <114049508+bahati10@users.noreply.github.com>
2023-11-29 18:47:26 +0200

    Setting up navbar (#6)

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { ThemeProvider } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import genTheme from "./theme/genTheme.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={genTheme}>
+      <CssBaseline />
       <App />
     </ThemeProvider>
   </React.StrictMode>
