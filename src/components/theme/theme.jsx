1cec8fd6154dc9a1e24fdf38c6b39e384ca607c7
bahati yves <114049508+bahati10@users.noreply.github.com>
2023-12-07 14:17:32 +0200

    Implementing main Theme and theme registry (#16)

---
+++ src/components/theme/theme.jsx
@@ -0,0 +1,32 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#EEE5E9",
+    },
+    secondary: {
+      main: "#000D11",
+    },
+  },
+  background: {
+    primary: {
+      main: "#000D11",
+    },
+  },
+  typography: {
+    h5: {
+      fontFamily: "Righteous",
+      color: "#000D11",
+    },
+    h6: {
+      fontFamily: "Poppins",
+      color: "#000D11",
+      fontSize: 14.5,
+      fontWeight: 600,
+      wordSpacing: 4,
+    },
+  },
+});
+
+export default theme;
