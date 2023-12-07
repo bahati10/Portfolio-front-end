5b1aa722c5a49c61b267f2af83c7fe2e791aa968
bahati yves <114049508+bahati10@users.noreply.github.com>
2023-12-07 14:17:32 +0200

    Implementing main Theme and theme registry (#16)

---
+++ testing.js
@@ -0,0 +1,9 @@
+// const [isLightMode, setLightMode] = useState(
+//     typeof window !== "undefined" &&
+//       window.localStorage.getItem("isLightMode") === "true"
+//   );
+
+
+//   if (typeof window !== "undefined") {
+//     window.localStorage.setItem("isLightMode", isLightMode);
+//   }
