995ff24e5f86c1fcb60321dbbecb4e1c93f45901
Bahati Yves <bahatiyves10@gmail.com>
2023-12-09 21:30:13 +0200

    Not found page implementation

---
+++ src/app/not-found.jsx
@@ -0,0 +1,55 @@
+import { Typography } from "@mui/material";
+import Link from "next/link";
+
+const Notfound = () => {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        backgroundColor: "#F3F4FC",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          textAlign: "center",
+        }}
+      >
+        <Typography
+          variant="h6"
+          sx={{
+            color: "#000D11",
+            fontSize: "5rem",
+          }}
+        >
+          404
+        </Typography>
+        <Typography
+          variant="h6"
+          sx={{
+            color: "#000D11",
+          }}
+        >
+          Page Not Found{" "}
+          <span>
+            {" "}
+            <Link
+              style={{
+                textDecoration: "none",
+              }}
+              href="/"
+            >
+              {" "}
+              Go back Home
+            </Link>
+          </span>
+        </Typography>
+      </div>
+    </div>
+  );
+};
+
+export default Notfound;
