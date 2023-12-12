be7194c4f7d8e40361a41f4c5e1b59ffa10e5684
bahati yves <114049508+bahati10@users.noreply.github.com>
2023-12-12 23:49:49 +0200

    Ft home page (#21)
    * Updating home page
    
    * Updating home page

---
+++ src/app/Home/page.jsx
@@ -0,0 +1,8 @@
+import { Button, Typography } from "@mui/material";
+import empty from "../../constants/empty.jpg";
+
+const News = () => {
+  return <></>;
+};
+
+export default News;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,13 @@
+import Navbar from "@/components/common/Navbar";
 import ThemeRegistry from "@/components/theme/ThemeRegistry";
 
 export default function RootLayout(props) {
   const { children } = props;
   return (
     <html lang="en">
-      <body suppressHydrationWarning={true}>
-        <ThemeRegistry options={{ key: 'mui' }}>
+      <body suppressHydrationWarning={true} style={{backgroundColor: "#F3F4FC"}}>
+        <ThemeRegistry options={{ key: "mui" }}>
+          <Navbar/>
           {children}
         </ThemeRegistry>
       </body>
