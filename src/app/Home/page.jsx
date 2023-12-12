918663cda1ea4b24a8658b1c71c35090f23d7d36
Bahati Yves <bahatiyves10@gmail.com>
2023-12-12 09:32:23 +0200

    Updating home page

--- src/app/Home/page.jsx
+++ src/app/Home/page.jsx
@@ -1,7 +1,7 @@
 import { Typography } from "@mui/material";
 
 const News = () => {
-  return <Typography variant="h6">This is a page for testing</Typography>;
+  return <Typography>Home</Typography>;
 };
 
 export default News;
