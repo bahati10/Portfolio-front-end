9c55b73cdcf8e80aa713ce708a9a8a193ce972ea
bahati yves <114049508+bahati10@users.noreply.github.com>
2023-12-12 23:49:49 +0200

    Ft home page (#21)
    * Updating home page
    
    * Updating home page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,55 +1,7 @@
-import { Typography } from "@mui/material";
-import Link from "next/link";
+import Not from "@/components/common/Notfound";
 
 const Notfound = () => {
-  return (
-    <div
-      style={{
-        height: "100vh",
-        width: "100vw",
-        backgroundColor: "#F3F4FC",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <div
-        style={{
-          textAlign: "center",
-        }}
-      >
-        <Typography
-          variant="h6"
-          sx={{
-            color: "#000D11",
-            fontSize: "5rem",
-          }}
-        >
-          4<span style={{ color: "green" }}>0</span>4
-        </Typography>
-        <Typography
-          variant="h6"
-          sx={{
-            color: "#000D11",
-          }}
-        >
-          Page Not Found{" "}
-          <span>
-            {" "}
-            <Link
-              style={{
-                textDecoration: "none",
-              }}
-              href="/"
-            >
-              {" "}
-              Go back Home
-            </Link>
-          </span>
-        </Typography>
-      </div>
-    </div>
-  );
+  return <Not statusCode={404} />;
 };
 
 export default Notfound;
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,22 @@
 import Navbar from "@/components/common/Navbar"
+import { Button } from "@mui/base"
+import { Typography } from "@mui/material"
 
 
 export default function Home() {
   return (
-    <></>
+    <>
+    <Button>
+      <Typography>Home</Typography>
+    </Button>
+
+    <Button>
+      <Typography>Home</Typography>
+    </Button>
+
+    <Button>
+      <Typography>Home</Typography>
+    </Button>
+    </>
   )
 }
